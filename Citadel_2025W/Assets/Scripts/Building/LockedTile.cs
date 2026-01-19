9417eb561a7fcf39e93faaf697f03c9178618ae5
KnockKnockP <hansu012345@gmail.com>
2026-01-19 17:24:25 +0900

    Add a method to unlock tiles.

--- Citadel_2025W/Assets/Scripts/Building/LockedTile.cs
+++ Citadel_2025W/Assets/Scripts/Building/LockedTile.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Citadel
 {
     public sealed class LockedTile : MonoBehaviour
     {
+        private static readonly List<LockedTile> LockedTiles = new();
+        
+        [SerializeField] private MeshRenderer meshRenderer;
+        
         [SerializeField] private bool locked;
         public bool Locked
         {
@@ -13,7 +19,7 @@ namespace Citadel
             {
                 locked = value;
 
-                if (locked)
+                if (!locked)
                 {
                     meshRenderer.materials[0] = originalMaterial;
                     Destroy(this);
@@ -21,7 +27,20 @@ namespace Citadel
             }
         }
 
-        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private int level = 1;
+        
         [SerializeField] private Material originalMaterial;
+        
+        public static void Unlock(int level)
+        {
+            LockedTile[] copy = LockedTiles.Where(lockedTile => lockedTile.locked && lockedTile.level == level).ToArray();
+            
+            foreach (LockedTile lockedTile in copy)
+                lockedTile.Locked = false;
+        }
+
+        private void OnEnable() => LockedTiles.Add(this);
+
+        private void OnDisable() => LockedTiles.Remove(this);
     }
 }
