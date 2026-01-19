a897601570de2ffbf63769cc1e0d418d2c7a8f96
RyanYang99 <diddbstjd0220@naver.com>
2026-01-20 01:51:44 +0900

    빌딩 카테고리 추가 및 에셋, 프리팹 생성

--- Citadel_2025W/Assets/Scripts/Builder/BuildingCategory.cs
+++ Citadel_2025W/Assets/Scripts/Builder/BuildingCategory.cs
@@ -11,9 +11,12 @@ namespace Citadel
     public enum BuildingSubCategory // 건물 종류 추가 
     {
         None, // Building이 아닐 경우
-        House,
-        Castle,
-        Warehouse,
-        Well
+        House, // 시민집
+        Castle, // 영주 성
+        Warehouse, // 벌목소 
+        Well, // 우물
+        Froge, // 대장간
+        Quarry, // 채석상
+        Market // 노점상(은행)
     }
 }
